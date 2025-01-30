e208ef0a8066524d91d34421cdc6cfb2f4766e57
Jahrome <yhwhscript@gmail.com>
2025-01-30 15:29:45 +0100

    feat: more examples

---
+++ modules-bak/Logger.js
@@ -0,0 +1,9 @@
+const LOG_LEVEL = 5; // 2 ERROR, 3 WARN, 4 INFO, 5 DEBUG
+
+export function error(...args) { if (LOG_LEVEL >= 2) console.error(...args); }
+
+export function warn(...args) { if (LOG_LEVEL >= 3) console.warn(...args); }
+
+export function log(...args) { if (LOG_LEVEL >= 4) console.log(...args); }
+// For DEBUG log level: verbose must be turned on in Dev Tools
+export function debug(...args) { if (LOG_LEVEL >= 5) console.debug(...args); }
